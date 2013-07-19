d0b608329dd4e32f6d5f5c86b0b8534addf26b24
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-07-19 14:10:48 -0400

    Add ecto

--- mix.exs
+++ mix.exs
@@ -20,6 +20,8 @@ defmodule SharedResources.Mixfile do
   defp deps do
     [ { :cowboy, github: "extend/cowboy" },
       { :dynamo, "0.1.0.dev", github: "elixir-lang/dynamo" },
-      {:apprentice,"0.0.1",[github: "joeyjoejoejr/apprentice"]}]
+      {:apprentice,"0.0.1",[github: "joeyjoejoejr/apprentice"]},
+      { :ecto, github: "elixir-lang/ecto" },
+      { :pgsql, github: "semiocast/pgsql" }]
   end
 end
