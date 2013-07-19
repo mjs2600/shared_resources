8f90bfb40d87ace8811b154ac001941fd9dac7cb
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-07-19 19:29:05 -0400

    Add twitter bootstrap and a main layout/view

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -6,6 +6,7 @@ defmodule ApplicationRouter do
     # You can comment the line below if you don't need
     # any of them or move them to a forwarded router
     conn.fetch([:cookies, :params])
+    conn.assign :layout, "application_layout"
   end
 
   # It is common to break your Dynamo in many
@@ -13,7 +14,10 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
+    conn = conn.assign(:resources, [
+      [name: "Van", location: "327 Main St", checked_out: nil],
+      [name: "Conference Room", location: "329 Main St", checked_out: "Joe Jackson"]
+    ])
     render conn, "index.html"
   end
 end
