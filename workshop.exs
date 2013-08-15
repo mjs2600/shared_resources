61bb1a6905cf953f477a9b3fce16f42d29ed9089
Joe Jackson <joe.jackson@scimendsolutions.com>
2013-08-15 16:11:08 -0400

    Install apprentice

---
+++ workshop.exs
@@ -0,0 +1,27 @@
+defmodule Apprentice.Workshop do
+  #return all the apprentices you would like to run
+  def apprentices do
+    [Apprentice.ExUnit] #Apprentice.Handlebars
+  end
+
+  def exunit_paths do
+    ["lib", "test"]
+  end
+
+  # def handlebars_template_paths do
+  #   ["tmp"]
+  # end
+
+  # def handlebars_output_path do
+  #   "tmp"
+  # end
+
+  # def scribe_template_paths do
+  #   ["tmp"]
+  # end
+
+  # def scribe_output_path do
+  #   "tmp"
+  # end
+
+end
