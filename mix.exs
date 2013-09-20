d0c1e511fba4ea2e44e2efaf6fc8fcf06486f0ec
Michael Stalker <michael.stalker@scimedsolutions.com>
2013-09-20 09:31:22 -0400

    Remove version number of dependencies
    Elixir removed a function that some dependencies were using. The newest
    versions of these dependencies don't rely on that missing function.

--- mix.exs
+++ mix.exs
@@ -19,9 +19,9 @@ defmodule SharedResources.Mixfile do
 
   defp deps do
     [ { :cowboy, github: "extend/cowboy" },
-      { :dynamo, "0.1.0-dev", github: "elixir-lang/dynamo" },
+      { :dynamo, github: "elixir-lang/dynamo" },
       { :apprentice, github: "joeyjoejoejr/apprentice" },
-      { :amnesia, "0.1.0", github: "meh/amnesia" },
+      { :amnesia, github: "meh/amnesia" },
       { :jsonex, "2.0", github: "marcelog/jsonex", tag: "2.0" } ]
   end
 end
