3f1aad445f991766c52e363ca033b171af816da8
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-09-20 14:18:22 -0400

    Add user queries

--- lib/shared_resources/user.ex
+++ lib/shared_resources/user.ex
@@ -1,2 +1,23 @@
 defmodule SharedResources.User do
+  use Amnesia
+  require Exquisite
+
+  def find_user_by_name(id) do
+    query = Exquisite.match SharedResources.Database.User,
+            where: id == id
+  end
+
+  def find_user_by_name(name) do
+    query = Exquisite.match SharedResources.Database.User,
+            where: name == name
+  end
+
+  defp find_with_query(query) do
+    Amnesia.transaction do
+      result = SharedResources.Database.User.select query
+      {_, [user | _], _} = result
+
+      user
+    end
+  end
 end
