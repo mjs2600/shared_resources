3c6b4a83321c216b877e2e016c522d172caf724b
Mike Simpson <mike.simpson@scimedsolutions.com>
2013-09-20 11:46:57 -0400

    Write new data to the database.

--- lib/shared_resources/database.ex
+++ lib/shared_resources/database.ex
@@ -20,18 +20,16 @@ defdatabase SharedResources.Database do
     def checked_out_by!(self) do
       User.read!(self.user_id)
     end
-    
+
     def check_in(self) do
       Amnesia.transaction do
-        self.user_id(nil)
-        self.write
+        self.user_id(nil).write
       end
     end
 
     def check_out(user_id, self) do
       Amnesia.transaction do
-        self.user_id(user_id)
-        self.write
+        self.user_id(user_id).write
       end
     end
   end
