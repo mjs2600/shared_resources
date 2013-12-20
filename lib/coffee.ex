4277a8775f248672f9a6d86e4969207c2e1490d7
Joe Peck <joe.peck@scimedsolutions.com>
2013-12-20 15:30:59 -0500

    Add warning if coffee-script is not installed.

--- lib/coffee.ex
+++ lib/coffee.ex
@@ -1,5 +1,7 @@
 defmodule Coffee do
   def run do
+    IO.puts "Compilin' coffee..."
     System.cmd("coffee -wj priv/static/js/compiled/application.js -c priv/static/coffee/*.coffee")
+    IO.puts "Please install coffee-script!"
   end
 end
