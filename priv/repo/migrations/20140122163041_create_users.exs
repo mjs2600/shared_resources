782dafd5e4e92d28cb459a0521b24a8b6692b6be
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-01-22 13:14:32 -0500

    Fix migration

--- priv/repo/migrations/20140122163041_create_users.exs
+++ priv/repo/migrations/20140122163041_create_users.exs
@@ -2,7 +2,7 @@ defmodule Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def up do
-    "CREATE TABLE IF NOT EXISTS users(id serial primary key, name character varying(255), email character varying(255), encrypted_password character varying(255), salt character varying(255))"
+    "CREATE TABLE IF NOT EXISTS users(id serial primary key, name character varying(255), email_address character varying(255), encrypted_password character varying(255), salt character varying(255))"
   end
 
   def down do
