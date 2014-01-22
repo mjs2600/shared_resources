541b7e257b7b4469beae9874909b546fe6c36aa3
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-01-22 14:59:47 -0500

    Move to string up a level

--- lib/shared_resources/user/password.ex
+++ lib/shared_resources/user/password.ex
@@ -1,18 +1,21 @@
 defmodule SharedResources.User.Password do
   def encrypt(password, salt) do
     {:ok, hash} = :bcrypt.hashpw(password, salt)
-    hash
+    string_hash = hash |> String.from_char_list!
+
+    string_hash
   end
 
   def gen_salt do
     {:ok, salt} = :bcrypt.gen_salt
+    string_salt = salt |> String.from_char_list!
 
-    salt
+    string_salt
   end
 
   def create(password) do
-    salt = gen_salt |> String.from_char_list!
-    encrypted_password = encrypt(password, salt) |> String.from_char_list!
+    salt = gen_salt
+    encrypted_password = encrypt(password, salt)
     {encrypted_password, salt}
   end
 end
