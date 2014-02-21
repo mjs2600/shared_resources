76511bfeaac680a24a44d3aca34809940dd11cef
Joe Peck <joe.peck@scimedsolutions.com>
2014-02-21 09:56:39 -0500

    Merging in changes from Mike

--- lib/repo.ex
+++ lib/repo.ex
@@ -2,7 +2,7 @@ defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def url do
-    "ecto://root@localhost/shared_resources"
+    "ecto://joepeck@localhost/shared_resources"
   end
 
   def priv do
